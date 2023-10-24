ebff1f7928484d46c2db78f80c8e2ddbab64f265
Alifia Azzahra <144238253+alfzzhr@users.noreply.github.com>
2023-10-24 15:05:11 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan1Nomor1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1Nomor1 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner(System.in);
+
+        int tahun;
+        System.out.println("Masukkan tahun: ");
+        tahun = input28.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
